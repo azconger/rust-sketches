8831a0eab5d88cd995b3cd8145738ebd93e44dab
April Conger <april.conger@stackhawk.com>
2025-02-15 11:03:38 -0700

    finished guessing game

--- bins/src/bin/guessing.rs
+++ bins/src/bin/guessing.rs
@@ -1,16 +1,32 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("Welcome to the Guess the Number!");
     let random_number = rand::rng().random_range(1..=100);
     println!("The random number is {random_number}. 🤫");
-    println!("Please guess a number > ");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("user input should be a string");
+    loop {
+        println!("Please guess a number between 1 and 100 > ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("user input should be a string");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(e) => {
+                println!("Error: {e:}");
+                continue
+            }
+        };
 
-    println!("You guessed: {}.", guess);
-
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("Too small! ☹️"),
+            Ordering::Equal => {
+                println!("You win! 🤩");
+                break
+            },
+            Ordering::Greater => println!("Too big! ☹️"),
+        }
+    }
 }
